d50c4d60fba85ee063b804bfd7521eb95de7d3ce
Wangggod <34480521+Wangggod@users.noreply.github.com>
2019-05-15 20:49:16 +0800

    用来读取一张图片并调用模型接口进行图像分类

---
+++ easydl.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+'''
+easydl图像分类
+'''
+import requests
+import base64
+import json
+AccessToken = '24.a635e7b3fd6b5c382df77c079539aece.2592000.1560497779.282335-16249190'
+url = 'https://aip.baidubce.com/rpc/2.0/ai_custom/v1/classification/model_1' + '?access_token='+ AccessToken
+
+filename = '1.jpg'
+headers = {'Content-Type' :'application/json'}
+
+with open(filename , 'rb') as file:
+    pic = base64.b64encode(file.read()).decode()
+data = {
+    'image':pic,
+    "top_num": 5
+}
+request = requests.post(url,headers= headers, data=json.dumps(data))
+print(json.loads(request.content))
