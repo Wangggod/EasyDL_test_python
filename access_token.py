aa9d3a9229e68df47c8a4dff989d3bfa1e5aff20
Wangggod <34480521+Wangggod@users.noreply.github.com>
2019-05-15 20:48:08 +0800

    通过申请发布模型时获得的AK和SK来获取access_token

--- access_token.py
+++ access_token.py
@@ -2,7 +2,7 @@ import urllib, urllib.request, sys
 import ssl
 
 # client_id 为官网获取的AK， client_secret 为官网获取的SK
-host = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=Em9lpYb87FFSf1oFWDOCuIxL&client_secret=qI6VPU0wzcfoMMFjN7g5sWsAMpGRZHPA'
+host = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=【官网获取的AK】&client_secret=【官网获取的SK】'
 request = urllib.request.Request(host)
 request.add_header('Content-Type', 'application/json; charset=UTF-8')
 response = urllib.request.urlopen(request)
